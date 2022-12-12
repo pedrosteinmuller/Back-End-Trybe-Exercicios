2e7ca555784616856ed6314061f8aad446ab9055
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2022-12-12 16:25:55 -0300

    exercitando teoria vista no course

---
+++ Backend/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-e-middlewares/src/routes/teamsRouter.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const validateTeam = require('../middlewares/validateTeam');
+const apiCredentials = require('../middlewares/apiCredentials');
+
+const router = express.Router();
+
+let nextId = 3;
+const teams = [
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
+];
+
+router.get('/', (req, res) => res.json(teams));
+
+router.use(apiCredentials);
+
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    res.json(team);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.post('/', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
+});
+
+router.put('/:id', validateTeam, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find(t => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+  }
+  res.sendStatus(200);
+});
+
+module.exports = router;
