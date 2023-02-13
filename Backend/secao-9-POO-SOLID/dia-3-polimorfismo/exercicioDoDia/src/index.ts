0f99f8dcfea7ae6f2ae6be0930cbf449435ec422
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-13 20:13:21 -0300

    feat: realizado exercicios do dia

---
+++ Backend/secao-9-POO-SOLID/dia-3-polimorfismo/exercicioDoDia/src/index.ts
@@ -0,0 +1,12 @@
+import Clube from './Clube';
+import QuadraFutebol from './QuadraFutebol';
+
+const trybeClube = new Clube();
+const quadraFutebol = new QuadraFutebol();
+trybeClube.adicionarQuadra(quadraFutebol);
+
+const dataReserva = new Date('2023-02-13');
+
+const reservarQuadraFutebol = trybeClube.buscarQuadra<QuadraFutebol>(0) 
+.reservar(dataReserva); 
+  console.log(reservarQuadraFutebol);
