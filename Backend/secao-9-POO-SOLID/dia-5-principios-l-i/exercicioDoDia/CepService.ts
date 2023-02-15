25d4b87333ccb6ccf71272a407e2c237f96f76ea
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-15 16:16:53 -0300

    feat: feito pratica de teorias do dia e realizado exercicio 1

---
+++ Backend/secao-9-POO-SOLID/dia-5-principios-l-i/exercicioDoDia/CepService.ts
@@ -0,0 +1,19 @@
+import FooCepAPI from '../exercicioDoDia/FooCepApi';
+
+class CepService {
+  private readonly cepApi: FooCepAPI;
+
+  constructor(cepApi: FooCepAPI) {
+    this.cepApi = cepApi;
+  }
+
+  addressByCep(cep: string, num: number) {
+    return this.cepApi.getAddressByCEP(cep, num);
+  }
+
+  cepByAddress(address: string, num: number) {
+    return this.cepApi.getCepByAddress(address, num);
+  }
+}
+
+export default CepService;
