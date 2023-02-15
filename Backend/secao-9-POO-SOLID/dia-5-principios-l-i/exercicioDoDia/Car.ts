715cf09141de709dfa29b9a46b0eee62bdb5ebe5
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-15 19:44:58 -0300

    feat: resolucao exercicio 6

--- Backend/secao-9-POO-SOLID/dia-5-principios-l-i/exercicioDoDia/Car.ts
+++ Backend/secao-9-POO-SOLID/dia-5-principios-l-i/exercicioDoDia/Car.ts
@@ -1,26 +1,5 @@
-import { IVehicle } from './interfaces';
+import { ICar } from './interfaces';
 
-class AirPlane implements IVehicle {
-  drive(): void {
-    throw new Error('I don\'t drive');
-  }
-
-  fly(): void {
-    console.log('Fly a airplane');
-  }
-}
-
-class Car implements IVehicle {
-  drive(): void {
-    console.log('Drive a car');
-  }
-
-  fly(): void {
-    throw new Error('I don\'t fly');
-  }
-}
-
-export default {
-  Car,
-  AirPlane
+export default class Car implements ICar {
+  drive(): void { console.log('Drive a car'); }
 }
